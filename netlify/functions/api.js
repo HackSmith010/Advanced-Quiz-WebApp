5825296374cdf239a32a75aae571738d76d2388f
HackSmith010 <anshujaiswal119@gmail.com>
2025-08-08 12:20:13 +0530

    small changes

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,6 @@
 import serverless from 'serverless-http';
-import app from '../../api/index.js';
+import appModule from '../../api/index.js';
+
+const app = appModule.default || appModule;
 
 export const handler = serverless(app);
